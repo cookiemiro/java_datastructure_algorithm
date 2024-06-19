50851874e9d861f166a1bc3f6c661d8df0b64d3d
cookiemiro <xodml9598@gmail.com>
2024-06-19 23:51:55 +0900

    두개 뽑아서 더하기: ArrayList와 for로 출력을 시도

---
+++ Array/SelectTwoNums.java
@@ -0,0 +1,40 @@
+package Array;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class SelectTwoNums {
+    public static void main(String[] args) {
+        int[] arr = new int[] {2, 1, 3, 4, 1};
+        int[] arr2 = {5, 0, 2, 7};
+
+        Arrays.sort(solution(arr));
+        Arrays.sort(solution(arr2));
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(arr2));
+    }
+
+    private static int[] solution(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            int tmp = arr[i];
+            for (int j = i + 1; j < arr.length; j++) {
+                int sum = tmp + arr[j];
+
+                if (list.contains(sum)) {
+                    continue;
+                }
+                list.add(sum);
+            }
+        }
+
+        // System.out.println(list.stream().mapToInt(i -> i));
+        // System.out.println(list.stream().mapToInt(i -> i).toString());
+
+        // ArrayList를 int[]로 변환하는 방법
+        // https://hoehen-flug.tistory.com/49
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
